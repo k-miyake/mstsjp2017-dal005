cb914ab1f59570eb21ef1c495f2772dd0f489b2e
Kazuyuki Miyake <miyake@zenarc.jp>
2017-10-31 16:12:24 +0900

    add Documents project

---
+++ Documents/PersonSample.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+
+namespace Documents
+{
+
+    public class PersonSample
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public string LastName { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public string FirstName { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public Address Address { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public string Title { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public Contact[] Contacts { get; set; }
+    }
+
+    public class Address
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Line1 { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public string Line2 { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public string City { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public string State { get; set; }
+        [JsonProperty(PropertyName = "id")]
+        public int Zip { get; set; }
+    }
+
+    public class Contact
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Email { get; set; }
+    }
+
+}
