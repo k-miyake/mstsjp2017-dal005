eada32a252944f561ebc3c44ea6e7f887d84219f
Kazuyuki Miyake <miyake@zenarc.jp>
2018-01-27 00:52:05 +0900

    Removed unused package

--- ChangeFeed/EventFeed.cs
+++ ChangeFeed/EventFeed.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 
 namespace ChangeFeed
