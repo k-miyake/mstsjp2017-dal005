1478a1ecea913b481ec6f3887aa12e11bfdee417
Kazuyuki Miyake <miyake@zenarc.jp>
2018-01-26 11:55:44 +0900

    Fixed PersonSample Class

--- Documents/PersonSample.cs
+++ Documents/PersonSample.cs
@@ -7,35 +7,35 @@ namespace Documents
     {
         [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
-        [JsonProperty(PropertyName = "id")]
+        [JsonProperty(PropertyName = "lastName")]
         public string LastName { get; set; }
-        [JsonProperty(PropertyName = "id")]
+        [JsonProperty(PropertyName = "firstName")]
         public string FirstName { get; set; }
-        [JsonProperty(PropertyName = "id")]
+        [JsonProperty(PropertyName = "address")]
         public Address2 Address { get; set; }
-        [JsonProperty(PropertyName = "id")]
+        [JsonProperty(PropertyName = "title")]
         public string Title { get; set; }
-        [JsonProperty(PropertyName = "id")]
+        [JsonProperty(PropertyName = "contacts")]
         public Contact2[] Contacts { get; set; }
     }
 
     public class Address2
     {
-        [JsonProperty(PropertyName = "id")]
+        [JsonProperty(PropertyName = "line1")]
         public string Line1 { get; set; }
-        [JsonProperty(PropertyName = "id")]
+        [JsonProperty(PropertyName = "line2")]
         public string Line2 { get; set; }
-        [JsonProperty(PropertyName = "id")]
+        [JsonProperty(PropertyName = "city")]
         public string City { get; set; }
-        [JsonProperty(PropertyName = "id")]
+        [JsonProperty(PropertyName = "state")]
         public string State { get; set; }
-        [JsonProperty(PropertyName = "id")]
+        [JsonProperty(PropertyName = "zip")]
         public int Zip { get; set; }
     }
 
     public class Contact2
     {
-        [JsonProperty(PropertyName = "id")]
+        [JsonProperty(PropertyName = "email")]
         public string Email { get; set; }
     }
 
